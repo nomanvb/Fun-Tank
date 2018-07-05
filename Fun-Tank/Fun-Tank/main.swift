05159cc64eb3b0e49f6d99af35b7859ad373a682
Ariful Ambia <ariful.ambia@gmail.com>
2018-07-06 02:07:03 +0600

    test code with verialbes

---
+++ Fun-Tank/Fun-Tank/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Fun-Tank
+//
+//  Created by Ariful Ambia on 7/6/18.
+//  Copyright © 2018 GTR. All rights reserved.
+//
+
+import Foundation
+
+var name: String = "Ariful Ambia"
+var age: Int = 40
+var dd: Double = 23432
+
+print("\(name) is \(age) old.")
+
+for i in 1...5{
+print("\(i)")
+}
